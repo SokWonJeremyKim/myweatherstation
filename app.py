3e0edc2c1e0c6704ef1089081263bbd4435260e6
SeopYoon <heeseop.y@gmail.com>
2016-08-29 19:47:09 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,55 @@
+#!flask/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+import time
+
+from flask import Flask, request
+from sqlalchemy import create_engine
+
+import model
+from model import Location, Session
+
+app = Flask(__name__)
+app.config.from_pyfile("config.py")
+
+app.db_engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"], pool_recycle = 3600, echo = False, encoding="utf-8")
+model.Base.metadata.bind = app.db_engine
+model.Session.configure(bind = app.db_engine)
+
+@app.route('/')
+def index():
+    return "hello"
+
+@app.route('/rainingHere/', methods = ['POST'])
+def reportRain():
+    json_content = request.get_json()
+    print json_content
+
+    sido = json_content.get("sido")
+    sigungu = json_content.get("sigungu")
+
+    Session.add(Location(sido, sigungu))
+    Session.commit()
+    return 'SUCCESSFUL'
+
+@app.route('/rainingLocations/', methods = ['GET'])
+def getrainingLocations():
+    rainingLocationsPastHour = Session.query(Location).filter(Location.reportedTime > (time.time() - 3600)).all()
+    if rainingLocationsPastHour is None:
+        print 'no raining locations' 
+        return 'NONE'
+    json_objects = []
+    for result in rainingLocationsPastHour:
+        json_objects.append('{"sido":"' + result.sido + '","sigungu":"' + result.sigungu + '","reportedTime":"' + str(result.reportedTime) + '"}')  
+    json_result = ','.join(json_objects)
+    json_result = '{"locations":[' + json_result + ']}'
+    return json_result
+
+@app.teardown_appcontext
+def close_session(exception = None):
+    Session.remove()
+
+if __name__ == '__main__':
+    model.Base.metadata.create_all()
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,20 @@
+import time
+
+from sqlalchemy import String, Integer, Float, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session, Query
+
+Base = declarative_base()
+Session = scoped_session(sessionmaker(query_cls = Query))
+
+class Location(Base):
+    __tablename__ = "locations"
+    id = Column(Integer, primary_key = True)
+    sido = Column(String)
+    sigungu = Column(String)
+    reportedTime = Column(Float)
+
+    def __init__(self, sido, sigungu):
+        self.sido = sido
+        self.sigungu = sigungu
+        self.reportedTime = time.time()
